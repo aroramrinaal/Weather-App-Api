bb51c38178118cef2cf3050c3ea16c68a8955485
Mrinaal Arora <90490253+aroramrinaal@users.noreply.github.com>
2024-04-04 00:09:26 +0000

    initalized the weather.py

---
+++ weather.py
@@ -0,0 +1,12 @@
+import requests
+
+def get_weather(api_key, city):
+    url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric'
+    response = requests.get(url)
+    if response.status_code == 200:
+        data = response.json()
+        return data
+    else:
+        print('Failed to fetch weather data')
+        return None
+    
